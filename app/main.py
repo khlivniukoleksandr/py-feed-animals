0f8f487524662ff05a1c699bb0f897fb4dd638fc
Khlivniuk Oleksandr <sasha2056083@gmail.com>
2025-05-14 16:03:08 +0300

    Fixed anotacions

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ class Animal:
         self.appetite = appetite
         self.is_hungry = is_hungry
 
-    def print_name(self) -> str:
+    def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
     def feed(self) -> object:
@@ -21,7 +21,7 @@ class Cat(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=3, is_hungry=is_hungry)
 
-    def catch_mouse(self) -> str:
+    def catch_mouse(self) -> None:
         print("The hunt began!")
 
 
@@ -29,7 +29,7 @@ class Dog(Animal):
     def __init__(self, name: str, is_hungry: bool = True) -> None:
         super().__init__(name, appetite=7, is_hungry=is_hungry)
 
-    def bring_slippers(self) -> str:
+    def bring_slippers(self) -> None:
         print("The slippers delivered!")
 
 
